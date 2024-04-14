bba18d19ec1daca009beef6cf1dd921a95b580d5
Dmitriy Sokolov <falcon22falcon22@gmail.com>
2024-04-14 14:14:34 +0500

    add test_main_page changed with Page Object Model

--- test_main_page.py
+++ test_main_page.py
@@ -1,19 +1,8 @@
-import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
-import time
+from .pages.main_page import MainPage
 
 
-def test_shopping_cart_existence(browser):
-    url = r"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(url)
-    button_element = browser.find_element(By.XPATH, r"//button[contains(@class,'btn-add-to-basket')]")
-    try:
-        button_element = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
-            (By.XPATH, r"//button[contains(@class,'btn-add-to-basket')]")))
-        button_search_result = True
-    except NoSuchElementException:
-        button_search_result = False
-    assert button_search_result
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
